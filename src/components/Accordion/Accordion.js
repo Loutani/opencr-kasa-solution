ec756d596889fb196111925397a2c58ae39d9b7d
Loutani mohammed es-salih <lotchomimo16saleh@gmail.com>
2022-10-13 19:30:12 +0100

    fix accordion design

--- src/components/Accordion/Accordion.js
+++ src/components/Accordion/Accordion.js
@@ -11,8 +11,8 @@ const Accordion = ({isOpen, title, children}) => {
     }
     
     return (
-        <div className='accordion' onClick={toggleAccordionHandler}>
-            <div className='accordion-title'>
+        <div className='accordion'>
+            <div className='accordion-title' onClick={toggleAccordionHandler}>
                 <p>{title}</p>
                 {
                     open ? <img alt='accordion-arrow' src={arrowUp} /> : <img alt='accordion-arrow' src={arrowDown} />

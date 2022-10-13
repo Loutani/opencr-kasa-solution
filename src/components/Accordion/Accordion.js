f95cfb61b04a3a8aac7732b6d0cdae0324851fe6
Loutani mohammed es-salih <lotchomimo16saleh@gmail.com>
2022-10-13 19:15:48 +0100

    integrate the accordion component

---
+++ src/components/Accordion/Accordion.js
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import arrowUp from './../../images/arrow-up.svg';
+import arrowDown from './../../images/arrow-down.svg';
+import './accordion.scss';
+
+const Accordion = ({isOpen, title, children}) => {
+    const [open, setOpen] = useState(isOpen);
+
+    const toggleAccordionHandler = () => {
+        setOpen(open => !open);
+    }
+    
+    return (
+        <div className='accordion' onClick={toggleAccordionHandler}>
+            <div className='accordion-title'>
+                <p>{title}</p>
+                {
+                    open ? <img alt='accordion-arrow' src={arrowUp} /> : <img alt='accordion-arrow' src={arrowDown} />
+                }
+            </div>
+            {
+                open && 
+                <div className='accordion-body'>
+                    {children}
+                </div>
+            }
+        </div>
+    )
+}
+
+export default Accordion;

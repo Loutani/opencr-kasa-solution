8fac3df4a6f05af1b7ae4809fc8ed36c7518402b
Loutani mohammed es-salih <lotchomimo16saleh@gmail.com>
2022-10-13 22:14:07 +0100

    integrate the star component

---
+++ src/components/Star/Star.js
@@ -0,0 +1,20 @@
+import './star.scss';
+import emptyStarImage from './../../images/empty_start.png';
+import filledStarImage from './../../images/fill_start.png';
+
+const Star = ({rating}) => {
+
+    let starItem = [];
+    
+    for(let i = 1; i <= 5; i++) {
+        starItem.push( <img alt='' src={(i <= rating) ? filledStarImage : emptyStarImage} /> )
+    }
+
+    return (
+        <div className='star'>
+            {starItem}
+        </div>
+    )
+}
+
+export default Star;

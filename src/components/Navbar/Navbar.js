54d04ea67ef31a1bae3e7730851f82b98e75bca1
Loutani mohammed es-salih <lotchomimo16saleh@gmail.com>
2022-10-15 20:07:18 +0100

    click on navbar logo redirect you to home page

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,17 @@
+import { useNavigate } from 'react-router-dom';
 import navBarLogo from './../../images/navbar_logo.svg';
 import './navbar.scss';
 
 const Navbar = ({children}) => {
+    const navigate = useNavigate();
+
+    const navigateToHome = () => {
+        navigate('/');
+    };
+
     return (
         <nav className='navbar'>
-            <div className='navbar-logo'>
+            <div onClick={() => navigateToHome()} className='navbar-logo'>
                 <img alt='kasa project logo' src={navBarLogo} />
             </div>
             <div className='navbar-content'>

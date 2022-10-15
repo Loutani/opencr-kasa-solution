d7ea53db4e3155c0a575e493720af3fa07e63813
Loutani mohammed es-salih <lotchomimo16saleh@gmail.com>
2022-10-15 18:40:29 +0100

    update thumb to handle Click

--- src/components/Thumb/Thumb.js
+++ src/components/Thumb/Thumb.js
@@ -1,8 +1,15 @@
+import { useNavigate } from 'react-router-dom';
 import './thumb.scss';
 
-const Thumb = ({imagePath, caption}) => {
+const Thumb = ({imagePath, caption, id}) => {
+    const navigate = useNavigate();
+
+    const thumbHandleClick = (id) => {
+        navigate(`detail/${id}`);
+    }
+
     return (
-        <div className='thumb'>
+        <div className='thumb'  onClick={() => thumbHandleClick(id)}>
             <img className='thumb-image' alt={caption} src={imagePath} />
             <div className='thumb-caption'>
                 <p>{caption}</p>
